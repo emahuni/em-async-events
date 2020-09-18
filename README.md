52c88aa9fccc5cf22ec0de51bf0c6b9a994ed400
Emmanuel Mahuni <emahuni@gmail.com>
2020-09-18 23:00:03 +0200

    perf: redefine methods and use a more modern scripting syntax
    docs: update documentation to include available options and features

--- README.md
+++ README.md
@@ -1,111 +1,166 @@
 # vue-hooked-async-events
 [![npm](https://img.shields.io/npm/v/vue-hooked-async-events.svg)](vue-hooked-async-events) ![npm](https://img.shields.io/npm/dt/vue-hooked-async-events.svg)
 
-This plugin is for easier Vue event bus management.
-By using standard event bus approach:
-```javascript
-    Vue.prototype.$eventBus = new Vue()
-    // or
-    export const EventBus = new Vue()
-```
-we create a new `Vue` instance with lots of unused methods and properties. `vue-hooked-async-events` creates simple object containing events-related functionality. 
-
+Easier and more useful Vue event bus management with zero dependencies.
 ## Installation
 ```javascript
-import Vue from 'vue'
-import HookedAsyncEvents from 'vue-hooked-async-events'
+import Vue from 'vue';
+import HookedAsyncEvents from 'vue-hooked-async-events';
 
-Vue.use(HookedAsyncEvents)
+Vue.use(HookedAsyncEvents);
 ```
 
 ## Events management
-Issue with using standard event bus is that you cannot just write `this.$eventBus.off()` inside component in order to unsubscribe only this component from all events it was subscribed to. Instead code above will remove all events from everywhere.
 
-This package is responsible for automatic event bus unsubscription when component is being destroyed. No more need to write something like:
+### Standard event bus approach & its issues
+To create a global event system the standard is to create an event bus:
+```javascript
+    Vue.prototype.$eventBus = new Vue();
+    // or
+    export const EventBus = new Vue();
+```
+
+#### Issues
+- Major issue with using standard event bus is that it creates a new `Vue` instance with lots of unused methods and properties. 
+- It is managed by Vue, thereby creating a lot of overhead.
+
+ `vue-hooked-async-events` on the other hand creates a simple object containing awesome events-related functionalities. 
+
+- You can't just write `this.$eventBus.off()` in order to unsubscribe only this component from all events it was subscribed to, that will instead remove all events from everywhere.
+- You remove events manually. You have to make sure you manually remove events in the component you listen from:
 ```javascript
     beforeDestroy() {
-        this.$eventBus.off('event-one', this.methodOne)
-        this.$eventBus.off('event-two', this.methodTwo)
-        this.$eventBus.off('event-three', this.methodThree)
-        this.$eventBus.off('event-four', this.methodFour)
+        this.$eventBus.off('event-one', this.methodOne);
+        this.$eventBus.off('event-two', this.methodTwo);
+        this.$eventBus.off('event-three', this.methodThree);
+        this.$eventBus.off('event-four', this.methodFour);
         // ... and so on
     }
 ```
+
+This package is responsible for automatic event bus unsubscription when component is being destroyed.
 Instead of above you should write:
 ```javascript
 ```
-Yes. Correct. Nothing. Plugin will handle all of this by itself, unsubscribing current component inside of its `beforeDestroy` hook.
-
-#### todo
-add async events and options documentation
+**Yes. Correct. Nothing.** Plugin will handle all of this by itself, unsubscribing current component inside of its `beforeDestroy` hook.
  
-### Methods
+## Methods
+There are several methods used to manage events.
+
+### Listening to events:
 Listening to event or events:
 ```javascript
-    created() {
-        this.$listenTo('some-event', this.eventCallback)
-        this.$listenTo(['second-event', 'third-event'], this.commonCallback)
-    }
-```
-It is possible to fire multiple callbacks (even for multiple events):
-```javascript
-    created() {
-        this.$listenTo('some-event', [this.eventCallbackOne, this.eventCallbackTwo])
-        this.$listenTo(['second-event', 'third-event'], [this.eventCallbackOne, this.eventCallbackTwo])
-    }
+created() {
+  this.$onEvent('some-event', this.eventCallback);
+  // listen once and remove
+  this.$onEvent('some-event', this.eventCallback1, { once: true });
+  this.$onceEvent('some-event', this.eventCallback2, { once: true });
+  // stop invoking other callbacks once this listener is executed
+  this.$onEvent('some-event', this.eventCallback3, { stop: true });
+  // automatically stop listening after 5000 milliseconds
+  this.$onEvent('some-event', this.eventCallback3, { expire: 5000 });
+  // multiple events being listen to by one callback
+  this.$onEvent(['second-event', 'third-event'], this.commonCallback);
+  // fire multiple callbacks (even for multiple events)
+  this.$onEvent('some-event', [this.eventCallback1, this.eventCallback2]);
+  this.$onEvent(['second-event', 'third-event'], [this.eventCallback1, this.eventCallback2]);
+},
+
+methods: {
+  eventCallback (payload, metadata) {
+    // payload is the data being passed by the event or from previous callback listener in the chain of callbacks
+    // for the event. EG: if this callback returns then the response is the next callback's payload.
+
+    // metadata is information about the event and the listener itself eg:
+    metadata == {
+      eventName: "opened-exlist-item", eventOptions: {/*opts passed to event*/},
+      callbackOptions: {/*opts passed to listener*/},
+      eventOrigin: VueComponent /*vue compo that emitted the event*/, listenersTally: 1
+    };
+  
+    return /*whatever response you want to the event if it's a hook; see below*/
+    // you can also return { payload, directives: {}}
+  }
+  eventCallback2 (payload, metadata) {
+    // you can also change how the event will behave by modifying the eventOptions or callbackOptions
+    // eg: stop invoking any subsequent callbacks on the event
+    metadata.callbackOptions.stop = true; // or 
+    metadata.eventOptions.stop = true;
+  }
+}
 ```
 
-Emitting event (example):
+### Emitting events:
+Emitting events is simple, but this package takes it to another level of usability with async events.
 ```javascript
-    methods: {
-        fireEvent() {
-            this.$emitEvent('some-event', { test: 'one' })
-        }
-    }
+methods: {
+  async fireEvents() {
+    this.$emitEvent('some-event', { test: 'one' });
+    // fire event callbacks in reverse order
+    this.$emitEvent('some-event2', { test: 'one' }, { reverse: true });
+    // why? EG: Consider the following components hierachy c=>c=>c=>c
+    // it will register events from the parent down to the last grandchild, 
+    // so when run, the event listeners' callbacks are run in the same order.
+    // To run in the other order from the last grandchild to the parent 
+    // emit the event with a reverse option. You will soon get why more below
+
+    // stop on the first listener callback
+    this.$emitEvent('some-event3', { test: 'one' }, { stop: true });
+    // linger for 5000ms for new listeners on the event. 
+    this.$emitEvent('some-event3', { test: 'one' }, { linger: 5000 });
+    // Why? burst race conditions, use with care
+    
+    // get info from the last listener (this is where you MAY need to use reverse invocation order)
+    const endResult = await this.$emitEvent('some-event', { test: 'one' }, { isHook: true, reverse: true });
+  }     
+}
 ```
 
+### Remove Event Listeners
 Removing event from events object for all listeners (example):
 ```javascript
     methods: {
         dontWannaListenAnymore() {
-            this.$eraseEvent('some-event') // now no component will listen to this event
-            this.$eraseEvent(['second-event', 'third-event'])
+            this.$eraseEvent('some-event'); // now no component will listen to this event
+            this.$eraseEvent(['second-event', 'third-event']);
         }
     }
 ```
 
-Unsubsribe from all events manually (example):
+### Remove Events manually (example):
 ```javascript
     methods: {
         leaveMeAlone() {
-            this.$fallSilent() // nice, but it is also done automatically inside "beforeDestroy" hook
+            // nice, but it is also done automatically inside "beforeDestroy" hook
+            this.$fallSilent(); 
         }
     }
 ```
-Remove specific callback for specific event (example): 
+### Remove specific callback for specific event (example): 
 ```javascript
     methods: {
         leaveMeWithoutSpecificCallback() {
-            this.$fallSilent('some-event', this.specificCallback)
+            this.$fallSilent('some-event', this.specificCallback);
         }
     }
 ```
-Remove array of callbacks for specific event (example):
+### Remove array of callbacks for specific event (example):
 ```javascript
     methods: {
         leaveMeWithoutSpecificCallbacks() {
-            this.$fallSilent('some-event', [this.callbackOne, this.callbackTwo])
+            this.$fallSilent('some-event', [this.callbackOne, this.callbackTwo]);
         }
     }
 ```
-Unsubscribe component from specific event or events (all component's callbacks for these events will be removed):
+### Unsubscribe component from specific event or events 
+- all component's callbacks for these events will be removed:
 ```javascript
     methods: {
         notListenToOne() {
-            this.$fallSilent('some-event')
+            this.$fallSilent('some-event');
         },
         notListenToMany() {
-            this.$fallSilent(['some-event', 'another-event'])
+            this.$fallSilent(['some-event', 'another-event']);
         }
     }
 ```
@@ -118,26 +173,27 @@ NOTE: use this feature at your own risk as it will warn you only for Vue basic p
     "$options", "$parent", "$root", "$children", "$refs", "$vnode", "$slots", "$scopedSlots", "$createElement", "$attrs", "$listeners", "$el"
 ```
 ```javascript
-    import Vue from 'vue'
-    import HookedAsyncEvents from 'vue-hooked-async-events'
+    import Vue from 'vue';
+    import HookedAsyncEvents from 'vue-hooked-async-events';
 
     Vue.use(HookedAsyncEvents, {
-        listenTo: '$hear',
+        onEvent: '$hear',
+        onceEvent: '$hearOnce',
         emitEvent: '$fireEvent',
         eraseEvent: '$deleteEvent',
         fallSilent: '$noMore'
-    })
+    });
 
     // later in component...
     created() {
-        this.$hear('some-event', this.callbackMethod)
+        this.$hear('some-event', this.callbackMethod);
     },
     methods: {
         doSmth() {
-            this.$fireEvent('some-event')
+            this.$fireEvent('some-event');
         },
         unsubscribe() {
-            this.$noMore('some-event')
+            this.$noMore('some-event');
         }
     }
 ```
@@ -146,5 +202,5 @@ NOTE: use this feature at your own risk as it will warn you only for Vue basic p
 Emmanuel Mahuni, changed a lot of things in the package to make even more awesome.
 
 #### Attribution
-This package was adopted from https://github.com/p1pchenk0/vue-handy-subscriptions
+This package was adopted from https://github.com/p1pchenk0/vue-handy-subscriptions 's idea, just had to make it another package as it departs a lot from the original.
 
--- index.js
+++ index.js
@@ -1,157 +1,63 @@
 'use strict';
 
-const vueReservedProps = ['$options', '$parent', '$root', '$children', '$refs', '$vnode', '$slots', '$scopedSlots', '$createElement', '$attrs', '$listeners', '$el'];
-
-function isEmpty (obj) {
-  return Object.keys(obj).length === 0;
-}
-
-function isArray (obj) {
-  return Array.isArray(obj);
-}
-
-function isCorrectCustomName (prop, options) {
-  if (vueReservedProps.includes(options[prop])) {
-    console.warn('[vue-handy-subscriptions]: ' + options[prop] + ' is used by Vue. Use another name');
-
-    return false;
-  }
-
-  return options && typeof options[prop] === 'string' && options[prop];
-}
-
-function addListener (_ref) {
-  let events = _ref.events,
-      eventName = _ref.eventName,
-      subscriberId = _ref.subscriberId,
-      callback = _ref.callback,
-      options = _ref.options;
-
-  // todo check if listener has an event lingering for it, if so then trigger this listener's event
-  (events[eventName] || (events[eventName] = [])).push({
-    subscriberId,
-    callback,
-    options
-  });
-
-  if (options.expiry) setTimeout(removeListeners, options.expiry, _ref);
-}
-
-async function runCallbacks (_ref2) {
-  let events = _ref2.events,
-      eventName = _ref2.eventName,
-      payload = _ref2.payload,
-      eventOptions = _ref2.options,
-      origin = _ref2.origin;
-
-  let event = events && events[eventName];
-  let cbTally = event && event.length;
-  let meta = {
-    eventOptions,
-    callbackOptions: {},
-    origin,
-    listenersTally:  cbTally
-  };
-  let res = payload;
-
-  if (cbTally) {
-    let ev;
-    if (eventOptions.reverse) ev = events[eventName].reverse(); else ev = events[eventName];
-
-    if (eventOptions.stop) cbTally = 1;
-
-    for (let listenerIndex = 0; listenerIndex < cbTally; listenerIndex++) {
-      let entry = ev[listenerIndex];
-
-      meta.callbackOptions = entry.options;
-
-      if (eventOptions.isAsync) {
-        res = await entry.callback(res, meta);
-      } else {
-        entry.callback(payload, meta);
-      }
-
-      if (entry.options.stop) break;
-    }
-  } //else {
-    // return new Promise(resolve => resolve(...cbArgs));
-  //}
-
-  return res;
-}
-
-function removeListeners (_ref3) {
-  let events = _ref3.events,
-      event = _ref3.event,
-      subscriberId = _ref3.subscriberId;
-
-  for (let listenerIndex = 0; listenerIndex < events[event].length; listenerIndex++) {
-    if (events[event][listenerIndex].subscriberId === subscriberId) {
-      events[event].splice(listenerIndex, 1);
-    }
-  }
-}
-
-function removeCallbacks (_ref4) {
-  let events = _ref4.events,
-      event = _ref4.event,
-      subscriberId = _ref4.subscriberId,
-      callback = _ref4.callback;
-
-  let indexOfSubscriber = events[event].findIndex(function (el) {
-    return el.subscriberId === subscriberId && el.callback === callback;
-  });
-
-  if (~indexOfSubscriber) {
-    events[event].splice(indexOfSubscriber, 1);
-  }
-}
-
-function removeGlobalEvent (_ref5) {
-  let events = _ref5.events,
-      eventName = _ref5.eventName;
-
-  for (let event in events) {
-    if (event === eventName) {
-      delete events[eventName];
-    }
-  }
-}
-
 export default {
+  /**
+   * install plugin
+   * @param Vue
+   * @param options
+   */
   install: function install (Vue, options = {}) {
-    let idSubsProp = isCorrectCustomName('idSubs', options) || '$idSubs';
-    let listenToProp = isCorrectCustomName('listenTo', options) || '$listenTo';
+    let asyncEventsProp = isCorrectCustomName('asyncEvents', options) || '$asyncEvents';
+    let onEventProp = isCorrectCustomName('onEvent', options) || '$onEvent';
+    let onceEventProp = isCorrectCustomName('onceEvent', options) || '$onceEvent';
     let emitEventProp = isCorrectCustomName('emitEvent', options) || '$emitEvent';
     let eraseEventProp = isCorrectCustomName('eraseEvent', options) || '$eraseEvent';
     let fallSilentProp = isCorrectCustomName('fallSilent', options) || '$fallSilent';
 
+    /**
+     * mix into vue
+     */
     Vue.mixin({
+      data () {return {
+        shouldFallSilent: true,
+      }},
       beforeCreate:  function beforeCreate () {
         this._uniqID = Math.random().toString(36).substr(2, 9);
-        this.shouldFallSilent = true;
       },
+
       beforeDestroy: function beforeDestroy () {
         if (this.shouldFallSilent) this.$fallSilent();
       }
     });
 
-    Vue.prototype[idSubsProp] = { _events: {} };
-    let events = Vue.prototype[idSubsProp]._events;
+    /**
+     * plugin local state
+     * @type {{_events: {}}}
+     */
+    Vue.prototype[asyncEventsProp] = { _events: {} };
+    let events = Vue.prototype[asyncEventsProp]._events;
 
-    /* subscribe to event */
-    Vue.prototype[listenToProp] = function (eventName, cb, options = { stop: false, expiry: 0 }) {
-      let ID = this._uniqID;
+    /**
+     * add event listener
+     * @param eventName
+     * @param callback
+     * @param options
+     */
+    Vue.prototype[onEventProp] = function (eventName, callback, options = { stop: false, expire: 0, once: false }) {
+      const args = {
+        events,
+        subscriberId:   this._uniqID,
+        listenerOrigin: this,
+        options
+      };
 
-      if (isArray(eventName) && isArray(cb)) {
+      if (isArray(eventName) && isArray(callback)) {
         for (let eventNameIndex = 0, len = eventName.length; eventNameIndex < len; eventNameIndex++) {
-          for (let callbackIndex = 0, _len = cb.length; callbackIndex < _len; callbackIndex++) {
+          for (let callbackIndex = 0, _len = callback.length; callbackIndex < _len; callbackIndex++) {
             addListener({
-              events,
-              eventName:    eventName[eventNameIndex],
-              subscriberId: ID,
-              callback:     cb[callbackIndex],
-              options
+              ...args,
+              eventName: eventName[eventNameIndex],
+              callback:  callback[callbackIndex]
             });
           }
         }
@@ -162,97 +68,279 @@ export default {
       if (isArray(eventName)) {
         for (let _eventNameIndex = 0, _len2 = eventName.length; _eventNameIndex < _len2; _eventNameIndex++) {
           addListener({
-            events:       events,
-            eventName:    eventName[_eventNameIndex],
-            subscriberId: ID,
-            callback:     cb,
-            options
+            ...args,
+            eventName: eventName[_eventNameIndex],
+            callback
           });
         }
 
         return;
       }
 
-      if (isArray(cb)) {
-        for (let _callbackIndex = 0, _len3 = cb.length; _callbackIndex < _len3; _callbackIndex++) {
+      if (isArray(callback)) {
+        for (let _callbackIndex = 0, _len3 = callback.length; _callbackIndex < _len3; _callbackIndex++) {
           addListener({
-            events:       events,
-            eventName:    eventName,
-            subscriberId: ID,
-            callback:     cb[_callbackIndex],
-            options
+            ...args,
+            eventName,
+            callback: callback[_callbackIndex]
           });
         }
       } else {
-        addListener({ events: events, eventName: eventName, subscriberId: ID, callback: cb, options });
+        addListener({
+          ...args,
+          eventName,
+          callback
+        });
       }
     };
 
-    /* fire event */
+
+    /**
+     * add event listener that only listens for event once and removed once executed
+     * @param eventName
+     * @param callback
+     * @param options
+     */
+    Vue.prototype[onceEventProp] = function (eventName, callback, options = { stop: false, expire: 0 }) {
+      options.once = true;
+      Vue.prototype[onEventProp](eventName, callback, options);
+    };
+
+    /**
+     * emit event and run callbacks subscribed to the event
+     * @param eventName
+     * @param payload
+     * @param options
+     * @return {Promise<*>}
+     */
     Vue.prototype[emitEventProp] = function (eventName, payload, options = {
       reverse: false,
       stop:    false,
       linger:  0,
-      isAsync:  false
+      isAsync: false
     }) {
-      console.debug(`index.js-170: () - this: %o`, this);
-      return runCallbacks({ events, eventName, payload, origin: this, options });
+      return runCallbacks({ events, eventName, payload, eventOrigin: this, options });
     };
 
-    /* remove event from events object */
+    /**
+     * remove event from events object
+     * @param eventName
+     */
     Vue.prototype[eraseEventProp] = function (eventName) {
       if (!isEmpty(events)) {
         if (isArray(eventName)) {
           for (let eventIndex = 0, len = eventName.length; eventIndex < len; eventIndex++) {
-            removeGlobalEvent({ events: events, eventName: eventName[eventIndex] });
+            removeGlobalEvent({ events, eventName: eventName[eventIndex] });
           }
         } else {
-          removeGlobalEvent({ events: events, eventName: eventName });
+          removeGlobalEvent({ events, eventName });
         }
       }
     };
 
-    /* unsubscribe from subscriptions */
-    Vue.prototype[fallSilentProp] = function (event, cb) {
-      let ID = this._uniqID;
+    /**
+     * unsubscribe from subscriptions
+     * @param event
+     * @param callback
+     */
+    Vue.prototype[fallSilentProp] = function (event, callback) {
+      const subscriberId = this._uniqID;
 
       if (!isEmpty(events)) {
-        if (event && event in events && typeof event === 'string' && !cb) {
-          removeListeners({ events: events, event: event, subscriberId: ID });
+        if (event && event in events && typeof event === 'string' && !callback) {
+          removeListeners({ events, event, subscriberId });
 
           return;
         }
 
-        if (event && isArray(event) && !cb) {
+        if (event && isArray(event) && !callback) {
           for (let eventIndex = 0, len = event.length; eventIndex < len; eventIndex++) {
-            removeListeners({ events: events, event: event[eventIndex], subscriberId: ID });
+            removeListeners({ events, event: event[eventIndex], subscriberId });
           }
 
           return;
         }
 
-        if (event && cb && isArray(cb) && event in events && events[event].length) {
-          for (let callbackIndex = 0, _len4 = cb.length; callbackIndex < _len4; callbackIndex++) {
-            removeCallbacks({ events: events, event: event, subscriberId: ID, callback: cb[callbackIndex] });
+        if (event && callback && isArray(callback) && event in events && events[event].length) {
+          for (let callbackIndex = 0, _len4 = callback.length; callbackIndex < _len4; callbackIndex++) {
+            removeCallbacks({ events, event, subscriberId, callback: callback[callbackIndex] });
           }
 
           return;
         }
 
-        if (event && cb && event in events && events[event].length) {
-          removeCallbacks({ events: events, event: event, subscriberId: ID, callback: cb });
+        if (event && callback && event in events && events[event].length) {
+          removeCallbacks({ events, event, subscriberId, callback });
 
           return;
         }
 
-        if (event && cb && typeof cb !== 'function') {
+        if (event && callback && typeof callback !== 'function') {
           return;
         }
 
         for (let _event in events) {
-          removeListeners({ events: events, event: _event, subscriberId: ID });
+          removeListeners({ events, event: _event, subscriberId });
         }
       }
     };
   }
 };
+
+
+
+/**
+ * assert if object is empty
+ * @param obj
+ * @return {boolean}
+ */
+function isEmpty (obj) {
+  return Object.keys(obj).length === 0;
+}
+
+/**
+ * assert if object is array
+ * @param obj
+ * @return {boolean}
+ */
+function isArray (obj) {
+  return Array.isArray(obj);
+}
+
+
+const vueReservedProps = ['$options', '$parent', '$root', '$children', '$refs', '$vnode', '$slots', '$scopedSlots', '$createElement', '$attrs', '$listeners', '$el'];
+
+/**
+ * assert if prop type is not reserved
+ * @param prop
+ * @param options
+ * @return {boolean|*}
+ */
+function isCorrectCustomName (prop, options) {
+  if (vueReservedProps.includes(options[prop])) {
+    console.warn('[vue-hooked-async-events]: ' + options[prop] + ' is used by Vue. Use another name');
+
+    return false;
+  }
+
+  return options && typeof options[prop] === 'string' && options[prop];
+}
+
+
+/**
+ * Add event listener
+ * @param events
+ * @param eventName
+ * @param subscriberId
+ * @param callback
+ * @param options
+ * @param listenerOrigin
+ */
+function addListener ({ events, eventName, subscriberId, callback, options, listenerOrigin }) {
+  // todo check if listener has an event lingering for it, if so then trigger this listener's event
+  (events[eventName] || (events[eventName] = [])).push({
+    eventName,
+    subscriberId,
+    listenerOrigin,
+    callback,
+    options
+  });
+
+  if (options.expire) setTimeout(removeListeners, options.expire, ...arguments);
+}
+
+/**
+ * Run event callbacks
+ * @param events
+ * @param eventName
+ * @param payload
+ * @param eventOptions
+ * @param eventOrigin
+ * @return {Promise<*>}
+ */
+async function runCallbacks ({ events, eventName, payload, eventOptions, eventOrigin }) {
+  let event = events && events[eventName];
+  let listenersTally = event && event.length;
+  let meta = {
+    eventName,
+    // make sure we don't mutate the actual options
+    eventOptions: Object.assign({}, eventOptions),
+    callbackOptions: {},
+    eventOrigin,
+    listenersTally
+  };
+  let res = payload;
+
+  if (listenersTally) {
+    let _listeners;
+    if (eventOptions.reverse) _listeners = events[eventName].reverse(); else _listeners = events[eventName];
+
+    if (eventOptions.stop) listenersTally = 1;
+    // console.debug(`[vue-hooked-async-events] index-66: runCallbacks() - eventName: %o, \neventOrigin: %o, \n_listeners: %o`, eventName, eventOrigin, _listeners.map(li => li.listenerOrigin._uid));
+    for (let listenerIndex = 0; listenerIndex < listenersTally; listenerIndex++) {
+      let listener = _listeners[listenerIndex];
+
+      // console.debug(`[vue-hooked-async-events] index-70: runCallbacks() - listener: %o`, listener.listenerOrigin._uid);
+
+      // make sure we don't mutate the actual options
+      meta.callbackOptions = Object.assign({}, listener.options);
+
+      if (eventOptions.isAsync) {
+        res = await listener.callback(res, meta);
+      } else {
+        listener.callback(payload, meta);
+      }
+
+      if (listener.options.stop || eventOptions.stop) break;
+      // todo cater for linger and expire changes from listener
+    }
+  }
+
+  return res;
+}
+
+/**
+ * remove all event listeners
+ * @param events
+ * @param event
+ * @param subscriberId
+ */
+function removeListeners ({ events, event, subscriberId }) {
+  for (let listenerIndex = 0; listenerIndex < events[event].length; listenerIndex++) {
+    if (events[event][listenerIndex].subscriberId === subscriberId) {
+      events[event].splice(listenerIndex, 1);
+    }
+  }
+}
+
+
+/**
+ * remove event callbacks
+ * @param events
+ * @param event
+ * @param subscriberId
+ * @param callback
+ */
+function removeCallbacks ({ events, event, subscriberId, callback }) {
+  let indexOfSubscriber = events[event].findIndex(function (el) {
+    return el.subscriberId === subscriberId && el.callback === callback;
+  });
+
+  if (~indexOfSubscriber) {
+    events[event].splice(indexOfSubscriber, 1);
+  }
+}
+
+
+/**
+ * remove event and all its callbacks
+ * @param events
+ * @param eventName
+ */
+function removeGlobalEvent ({ events, eventName }) {
+  for (let event in events) {
+    if (event === eventName) {
+      delete events[eventName];
+    }
+  }
+}
