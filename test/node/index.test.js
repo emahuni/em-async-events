11ec756aa4843253caca99f53a0b671a45e40a7b
Emmanuel Mahuni <emahuni@gmail.com>
2021-08-06 12:59:44 +0200

    tests: add emitEvent and onEvent tests

--- test/node/index.test.js
+++ test/node/index.test.js
@@ -64,4 +64,65 @@ describe(`# em-async-events`, function () {
   });
   
   
+  
+  describe(`# emitEvent() `, function () {
+    let vow, payload = 'emit-payload';
+    
+    beforeAll(async function () {
+      vow = ae.emitEvent('once-event', payload);
+    });
+    
+    it(`returned a promise that is not yet resolved`, async function () {
+      expect(vow).to.be.an.instanceof(Promise);
+    });
+    
+    describe(`# onceEventSpy ()`, function () {
+      it(`ran once and listener was removed`, async function () {
+        expect(onceEventSpy).to.have.been.calledOnce;
+        expect(ae.hasListener('once-event')).to.be.false;
+      });
+      
+      it(`called with appropriate arguments.`, async function () {
+        expect(onceEventSpy).to.have.been.calledWith(payload, sinon.match({
+          eventMeta:       sinon.match.object,
+          listenerOptions: sinon.match.object,
+          extra:           undefined,
+        }));
+      });
+      
+      it(`return the appropriate response`, async function () {
+        expect(onceEventSpy).to.have.returned(onceEventSpyResponse);
+      });
+    });
+    
+    describe(`# lingering event`, function () {
+      it(`has once-event lingering event`, async function () {
+        expect(ae.hasLingeringEvent('once-event')).to.be.true;
+      });
+      
+      it.skip(`returned a promise with result of last callback`, async function () {
+        expect(vow).to.be.an.instanceof(Promise);
+      });
+    });
+  });
+  
+  
+  
+  
+  describe(`# onEvent() - returns a promise that resolves to event payload`, function () {
+    let vow;
+    beforeAll(async function () {
+      vow = ae.onEvent('async-event', asyncEventSpy);
+    });
+    
+    it(`returned a promise`, async function () {
+      expect(ae.hasListener('async-event')).to.be.true;
+      expect(vow).to.be.an.instanceof(Promise);
+    });
+    
+    it.skip(`promise resolves to event payload`, async function () {
+    
+    });
+  });
+  
 });
